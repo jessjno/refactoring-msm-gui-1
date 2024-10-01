b58680f71431ec1a3446edf566ac9fbad96f28fa
Jessica JnoBaptiste <86270295+jessjno@users.noreply.github.com>
2024-10-01 16:40:51 +0000

    completed refactoring

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@
 #
 # Table name: movies
 #
-#  id          :integer          not null, primary key
+#  id          :bigint           not null, primary key
 #  description :text
 #  duration    :integer
 #  image       :string
@@ -13,4 +13,26 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  belongs_to :director
+  has_many :characters
+  # def director
+  #   my_director_id = self.director_id
+    
+  #   matching_directors = Director.where({ :id => my_director_id })
+
+  #   the_director = matching_directors.at(0)
+    
+  #   return the_director
+  # end
+
+  def title_with_year
+    return "#{self.title} (#{self.year})"
+  end
+  def characters
+    my_id = self.id
+
+    matching_characters = Character.where({ :movie_id => my_id })
+
+    return matching_characters
+  end
 end
